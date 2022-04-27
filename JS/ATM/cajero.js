1caa912e8315c7d10e0ecc6889cd10b5e879ceff
Sebastian Castro <sebascastrom95@gmail.com>
2022-04-27 00:40:12 -0500

    Updating variables, changing var for let or const on ATM (cajero.js - class.js

--- JS/ATM/cajero.js
+++ JS/ATM/cajero.js
@@ -1,21 +1,21 @@
-var saldo = document.getElementById('sld');
-var txxt = document.getElementById('txt');
-var button = document.getElementById('boton');
+let saldo = document.getElementById('sld');
+let txxt = document.getElementById('txt');
+const button = document.getElementById('boton');
 button.addEventListener('click', mostrar);
-var papeles, divi, dinero;
+let papeles, divi, dinero;
 var saldo1 = parseInt(0);
-var entregado = [];
+let entregado = [];
 
-for (var xx of caja) {
+for (let xx of caja) {
   var saldo1 = saldo1 + (xx.valor * xx.cantidad);
   saldo.innerHTML = 'Dinero disponible en cajero : $ ' + saldo1;
 }
 
 function mostrar() {
-  var dibujar = [];
-  var valorr = document.getElementById('txtdinero');
+  let dibujar = [];
+  let valorr = document.getElementById('txtdinero');
   dinero = parseInt(valorr.value);
-  for (var b of caja) {
+  for (let b of caja) {
     var saldo1 = parseInt(0);
     if (dinero > 0) {
       divi = Math.floor(dinero / b.valor);
@@ -25,24 +25,24 @@ function mostrar() {
         papeles = divi;
       }
       b.cantidad = b.cantidad - papeles;
-      for (var i = 0; i < papeles; i++) {
+      for (let i = 0; i < papeles; i++) {
         dibujar.push(new Billete(b.valor, 1));
       }
       entregado.push(new Billete(b.valor, papeles));
       dinero -= (b.valor * papeles);
     }
-    for (var xx of caja) {
+    for (let xx of caja) {
       var saldo1 = saldo1 + (xx.valor * xx.cantidad);
       saldo.innerHTML = 'Dinero disponible en cajero : $ ' + saldo1;
     }
   }
 
   if (dinero == 0) {
-    // for (var n of entregado) {
+    // for (let n of entregado) {
     //   if (n.cantidad > 0) {
     //     txxt.innerHTML += n.cantidad + ' billetes de $' + n.valor + '<br/>';
     txxt.innerHTML += '<strong>Se ha entregado: </strong></br>';
-    for (var y of dibujar) {
+    for (let y of dibujar) {
       y.imprimir();
     }
     txxt.innerHTML += '<hr/>'
--- JS/ATM/class.js
+++ JS/ATM/class.js
@@ -10,13 +10,13 @@ class Billete {
   }
 }
 
-var imagenes = []
+let imagenes = []
 imagenes['100'] = '100.png';
 imagenes['50'] = '50.png';
 imagenes['20'] = '20.png';
 imagenes['10'] = '10.png';
 
-var caja = [];
+let caja = [];
 caja.push(new Billete(100, 5));
 caja.push(new Billete(50, 6));
 caja.push(new Billete(20, 7));
