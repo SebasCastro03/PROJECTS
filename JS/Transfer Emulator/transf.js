3b7e33324d6028e39ceb5b2535da128b9d1bc542
Sebastian Castro <sebascastrom95@gmail.com>
2022-04-27 00:57:25 -0500

    Updating variables, changing var for let or const on Transfer emulator(transf.js)

--- JS/Transfer Emulator/transf.js
+++ JS/Transfer Emulator/transf.js
@@ -1,28 +1,28 @@
-var bo = document.getElementById('borigen');
-var bd = document.getElementById('bdestino');
-var boton = document.getElementById('transferir');
-var saldo = document.getElementById('saldo');
-var saldoini = document.getElementById('sinicial');
-var saldofin = document.getElementById('sfinal');
-var comision = document.getElementById('comision1');
-var vr = document.getElementById('vtransf');
+let bo = document.getElementById('borigen');
+let bd = document.getElementById('bdestino');
+const boton = document.getElementById('transferir');
+let saldo = document.getElementById('saldo');
+let saldoini = document.getElementById('sinicial');
+let saldofin = document.getElementById('sfinal');
+let comision = document.getElementById('comision1');
+let vr = document.getElementById('vtransf');
 
 boton.addEventListener("click", operacion);
 
 function operacion() {
-  var banco_cliente = bo.value;
-  var valor = parseInt(vr.value);
-  var cuenta_cliente = true;
-  var saldo_cliente = parseInt(saldo.value);
-  var banco_destino = bd.value;
-  var cuenta_destino = true;
-  var hora = 19;
+  let banco_cliente = bo.value;
+  let valor = parseInt(vr.value);
+  const cuenta_cliente = true;
+  let saldo_cliente = parseInt(saldo.value);
+  let banco_destino = bd.value;
+  const cuenta_destino = true;
+  let hora = 19;
 
   if (cuenta_cliente && cuenta_destino) {
     if ((hora >= 9 && hora <= 12) || (hora >= 15 && hora <= 20)) {
       saldoini.innerHTML = 'Saldo incial: $ ' + saldo_cliente;
       if (banco_cliente == banco_destino) {
-        var vr_comision = parseInt(0);
+        let vr_comision = parseInt(0);
         comision.innerHTML = 'Valor comision transferencia: $ ' + vr_comision;
         if ((vr_comision + valor) <= saldo_cliente) {
           saldo_cliente -= vr_comision + valor;
@@ -33,10 +33,10 @@ function operacion() {
         }
       }
       else {
-        var vr_comision = parseFloat(valor * 0.05);
-        comision.innerHTML = 'Valor comision transferencia: $ ' + vr_comision + 
-        '</br> (Comision de traslado a otros bancos equivalente al 5%)';
+        let vr_comision = parseFloat(valor * 0.05);
         if ((vr_comision + valor) <= saldo_cliente) {
+          comision.innerHTML = 'Valor comision transferencia: $ ' + vr_comision + 
+          '</br> (Comision de traslado a otros bancos equivalente al 5%)';
           saldo_cliente -= vr_comision + valor;
           saldofin.innerHTML = 'Saldo final: $ ' + saldo_cliente;
         }
